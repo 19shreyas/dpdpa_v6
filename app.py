8ab731769869576e4ad66bc16dbe8be7d49da438
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-15 02:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -490,7 +490,7 @@ elif menu == "Policy Compliance Checker":
                         
                             st.markdown("### 🔍 Matched Details:")
                             for detail in result["Matched Details"]:
-                                st.markdown(f"** {detail['Checklist Item']}** — `{detail['Status']}`  \n> _{detail['Justification']}_")
+                                
                                 status_color = {
                                     "explicitly mentioned": "#198754",  # green
                                     "partially mentioned": "#FFC107",   # yellow
